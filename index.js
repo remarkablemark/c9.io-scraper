5b3604927c726a5ea283abd6f58469f97727f61d
Mark <mark@remarkablemark.org>
2019-12-10 22:57:15 -0500

    feat(index): execute login step

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Builder } = require('selenium-webdriver');
+
+const LoginPage = require('./pages/login');
+
+(async function() {
+  let driver;
+
+  try {
+    driver = await new Builder().forBrowser('chrome').build();
+
+    // login
+    const login = new LoginPage(driver);
+    await login.login();
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await driver.quit();
+  }
+})();
