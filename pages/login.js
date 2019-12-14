cdb60391d463009aa7a4383117694d6c919cd9ac
Mark <mark@remarkablemark.org>
2019-12-13 23:44:41 -0500

    fix(pages): improve wait until condition to when title changes

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,12 @@
-const { Key, until } = require('selenium-webdriver');
+const { until } = require('selenium-webdriver');
 
 class LoginPage {
   BASE_URL = 'https://c9.io/';
   LOGIN_URL = this.BASE_URL + 'login';
-  WORKSPACES_URL = this.BASE_URL + process.env.USERNAME;
 
   USERNAME_INPUT_LOCATOR = { id: 'id-username' };
   PASSWORD_INPUT_LOCATOR = { id: 'id-password' };
+  SUBMIT_BUTTON_LOCATOR = { css: 'button[type="submit"]' };
 
   /**
    * @param {ThenableWebDriver} driver
@@ -27,9 +27,12 @@ class LoginPage {
     await username.sendKeys(process.env.USERNAME);
 
     const password = await this.driver.findElement(this.PASSWORD_INPUT_LOCATOR);
-    await password.sendKeys(process.env.PASSWORD, Key.ENTER);
+    await password.sendKeys(process.env.PASSWORD);
 
-    await this.driver.wait(until.urlIs(this.WORKSPACES_URL));
+    const submit = await this.driver.findElement(this.SUBMIT_BUTTON_LOCATOR);
+    await submit.click();
+
+    await this.driver.wait(until.titleIs(process.env.USERNAME));
   }
 }
 
