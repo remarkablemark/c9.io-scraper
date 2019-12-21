ac26f72620eebf878355a1a2bbdb3de044574d40
Mark <mark@remarkablemark.org>
2019-12-21 15:51:09 -0500

    feat(index): execute prepare to download from workspaces page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { Builder } = require('selenium-webdriver');
 
 const LoginPage = require('./pages/login');
+const WorkspacesPage = require('./pages/workspaces');
 
 (async function() {
   let driver;
@@ -10,9 +11,11 @@ const LoginPage = require('./pages/login');
   try {
     driver = await new Builder().forBrowser('chrome').build();
 
-    // login
     const login = new LoginPage(driver);
     await login.login();
+
+    const workspaces = new WorkspacesPage(driver);
+    await workspaces.prepareToDownload();
   } catch (err) {
     console.error(err);
   } finally {
