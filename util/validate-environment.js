b82e070697688e0a4108b2c7aa7934639b2201fb
Mark <mark@remarkablemark.org>
2019-12-29 14:26:34 -0500

    feat(util): validate required environment variables are set

--- util/validate-environment.js
+++ util/validate-environment.js
@@ -14,6 +14,20 @@ const validateEnvironment = () => {
   } else {
     console.log(`Node version: ${process.version}`);
   }
+
+  if (!process.env.USERNAME) {
+    console.log('Error: Missing environment variable `USERNAME`');
+    process.exit();
+  } else {
+    console.log(`USERNAME: ${process.env.USERNAME}`);
+  }
+
+  if (!process.env.PASSWORD) {
+    console.error('Error: Missing environment variable `PASSWORD`');
+    process.exit();
+  } else {
+    console.log(`PASSWORD: ${'*'.repeat(process.env.PASSWORD.length)}`);
+  }
 };
 
 module.exports = validateEnvironment;
